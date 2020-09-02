e96d42647a70575e22c5bf4ad67a2fc81ba15378
Arthur Carson <rthrcrsn@gmail.com>
2020-09-02 13:25:02 -0500

    fixed boilerplate

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ console.log("It's a me server.js")
 // Create an express app
 const app = express();
 
+// Serve static files from server/public folder
+app.use(express.static("server/public"))
+
 // Listen for requests
 const port = 3000;
 app.listen(port, function() {
