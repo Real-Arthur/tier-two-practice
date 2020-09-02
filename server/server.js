6b13d23dc54ccc5191e274ca730708c2ffa9fa6b
Arthur Carson <rthrcrsn@gmail.com>
2020-09-02 13:37:01 -0500

    Sent activities to server

--- server/server.js
+++ server/server.js
@@ -8,6 +8,34 @@ const app = express();
 // Serve static files from server/public folder
 app.use(express.static("server/public"))
 
+// Define a list of activities for my kid
+let activities = [
+    {
+        activity: "Typing practices",
+        isScreenTime: true,
+        type: "Mind Exercises"
+    },
+    {
+        activity: "Bike around the lake",
+        isScreenTime: false,
+        type: "Body Break"
+    },
+    {
+        activity: "FB Chat with a friend",
+        isScreenTime: true,
+        type: "Social"
+    },
+
+];
+
+// Endpoint:
+// GET /activities
+// Respond with a list of activities
+app.get('/activities', function(req, res) {
+    // Send back the array of activities
+    res.send(activities);
+});
+
 // Listen for requests
 const port = 3000;
 app.listen(port, function() {
